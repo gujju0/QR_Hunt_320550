007387cc223b33906657010a5772e2230cf07601
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:19:08 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "'.MOV' extension refers usually to what kind of file?",
+    answer: "Algo",
+    options: [
+      "AV002-A",
+      "AV109-D",
+      "AV006-B",
+      "AV113-B"
+    ]
+  },
+  {
+    numb: 2,
+    question: "In which decade was the SPICE simulator introduced?",
+    answer: "Macbook",
+    options: [
+      "AV003-C",
+      "AV110-A",
+      "AV005-C",
+      "AV112-A"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Most modern TV's draw power even if turned off. The circuit the power is used in does what function?",
+    answer: "Git hub",
+    options: [
+      "AV004-C",
+      "AV011-D",
+      "AV104-C",
+      "AV111-A"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which is a type of Electrically-Erasable Programmable Read-Only Memory?",
+    answer: "Mit",
+    options: [
+      "AV001-A",
+      "AV108-C",
+      "AV007-A",
+      "AV114-C"
+    ]
+  },
+  {
+    numb: 5,
+    question: "The purpose of choke in tube light is ?",
+    answer: "Uber",
+    options: [
+      "AV008-D",
+      "AV101-C",
+      "AV014-D",
+      "AV107-C"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "'.MPG' extension refers usually to what kind of file?",
+    answer: "Big data",
+    options: [
+      "AV009-B",
+      "AV102-D",
+      "AV013-C",
+      "AV106-A"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who is largely responsible for breaking the German Enigma codes, created a test that provided a foundation for artificial intelligence?",
+    answer: "Array",
+    options: [
+      "AV010-B",
+      "AV103-D",
+      "AV012-A",
+      "AV105-D"
+    ]
+  },
+];
